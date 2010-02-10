f6ae5bf8a9948d5daac44533831ed7fc33743103
Chris Anderson <jchris@apache.org>
2010-02-10 12:12:58 -0800

    task state and done tasks on user pages

--- _attachments/script/tasks.js
+++ _attachments/script/tasks.js
@@ -33,7 +33,8 @@ $.couch.app(function(app) {
               name : v.authorProfile && v.authorProfile.name,
               name_uri : v.authorProfile && encodeURIComponent(v.authorProfile.name),
               futon_path : "/_utils/document.html?"+[app.db.name,r.id].map(encodeURIComponent).join('/'),
-              id : encodeURIComponent(r.id)
+              id : encodeURIComponent(r.id),
+              state : v.state
               // todo this should be handled in dom-land / evently
               // we can use this id as a handle for automatically updating non-top rows
               // based on changes from documents
--- views/users-tasks/map.js
+++ views/users-tasks/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if (doc.type == "task" && doc.state != "done" && doc.authorProfile && doc.authorProfile.name) {
+  if (doc.type == "task" && doc.authorProfile && doc.authorProfile.name) {
     emit([doc.authorProfile.name, doc.created_at], doc)
   }
 }
