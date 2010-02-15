a702404d2da1732cf0c19cb5319d04904443d22c
Chris Anderson <jchris@apache.org>
2010-02-15 13:07:02 -0800

    edit in place works

--- vendor/couchapp/evently/docs/topic/after.js
+++ vendor/couchapp/evently/docs/topic/after.js
@@ -7,16 +7,21 @@ function() {
       var code = $(this);
       var js = code.text();
       var o = s.offset();
-      var r = js.match(/\$\(\"\#(.*)\"\)\.evently/);
+      var r = js.match(/\$\(\"\#(.*)\"\)/);
       if (r) {
         // $.log(o)
-        var example = $('<div class="example"><a class="run" href="#'+r[1]+'">run <strong>#'+r[1]+'</strong></a><div id="'+r[1]+'"><strong>#'+r[1]+'</strong> output will be here</div></div>');
+        var id = r[1];
+        code.attr({id:"code-"+id});
+        var example = $('<div class="example"><a class="run" href="#'+id+'">run <strong>#'+id+'</strong></a><div id="'+id+'"><strong>#'+id+'</strong> output will be here</div></div>');
         $("a.run", example).click(function(e) {
           e.preventDefault();
+          var jc = $("#code-"+id);
+          $.log(jc)
+          var js = $('textarea',jc).val() || jc.text();
           try {
             eval(js);            
           } catch (e) {
-            $('#'+r[1]).html('<p>Error running #'+r[1]+' code block:</p><p><pre>'+e.toSource()+'</pre></p>');
+            $('#'+id).html('<p>Error running #'+id+' code block:</p><p><pre>'+e.toSource()+'</pre></p>');
           }
         });
         example.offset({
@@ -25,6 +30,18 @@ function() {
         example.width(s.width()*0.75);
         code.before(example);
       }
+      // setup the editor
+      var edit = $('<a href="#edit">edit code</a>');
+      edit.click(function() {
+        var code = $(this).next('pre');
+        var js = code.text();
+        var lines = js.split('\n').length;
+        var ta = $('<textarea rows="'+lines+'" class="code"></textarea>');
+        ta.text(js);
+        code.replace(ta);
+        return false;
+      });
+      code.before(edit);
     });    
   }, 10);
 };
